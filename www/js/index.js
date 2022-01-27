c8604cb29ac9a79696925d350ac9f95a093c8c45
Pat Osorio <pat@smartwave.ph>
2022-01-27 16:54:19 +0800

    Added HUD Controller  - Virtual Joystick  - A + B buttons

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,19 @@
+import VirtualJoystick from '../node_modules/phaser3-rex-plugins/plugins/virtualjoystick.js';
+import Button from '../node_modules/phaser3-rex-plugins/plugins/button.js';
 
 var deviceWidth = 844;//innerWidth * window.devicePixelRatio;
 var deviceHeight = 390; //innerHeight * window.devicePixelRatio;
 var config = {
     type: Phaser.WEBGL,
-    // width: deviceWidth,
-    // height: deviceHeight,
     scale: {
         mode: Phaser.Scale.FIT,
         parent: 'phaser-example',
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: deviceWidth,
         height: deviceHeight
+    },
+    input: {
+        activePointers: 3 
     },
     physics: {
         default: 'arcade',
@@ -26,32 +29,24 @@ var config = {
     plugins: {
         scene: [
             { key: 'SpinePlugin', plugin: window.SpinePlugin, mapping: 'spine' }
-        ],
-        // global: [{
-        //     key: 'rexVirtualJoystick',
-        //     plugin: window.VirtualJoystickPlugin,
-        //     start: true
-        // }]
+        ]
     }
 };
 
 var game = new Phaser.Game(config);
 
 var cursors;
+var joystickCursors;
 var player;
+var isJumping;
 
 function preload() {
-    // this.scale.lockOrientation('landscape')
-
     this.load.image('ground', './img/platform.png');
     this.load.image('background', './img/city.png');
 
     // Load Spineboy
     this.load.setPath('./img/spineboy/');
     this.load.spine('sb', 'demos.json', [ 'atlas1.atlas' ], true);
-
-    // Load Joystick
-    // this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true);
 }
 
 function create() {
@@ -64,9 +59,7 @@ function create() {
 
     var spineBoy = this.add.spine(deviceWidth*0.2, deviceHeight/4, 'sb.spineboy', 'idle', true);
     
-    // spineBoy.setOffset(0, 0)
     spineBoy.setInteractive();
-    console.log(spineBoy.getAnimationList());
     console.log(spineBoy)
     player = this.physics.add.existing(spineBoy).setScale(0.15);
 
@@ -74,10 +67,29 @@ function create() {
     player.body.setCollideWorldBounds(true);
     
     this.input.enableDebug(player, 0xff00ff);
-    
 
-    // var joyStick = this.plugins.get('rexVirtualJoystick').addPlayer(this, config);
-    // console.log(joyStick.pointerX)
+    var joyStick = new VirtualJoystick(this, {
+        x: deviceWidth * 0.10,
+        y: deviceHeight * 0.75,
+        radius: 50
+    });
+
+    joystickCursors = joyStick.createCursorKeys();
+
+    createBtn(this, {
+        x: deviceWidth * 0.8,
+        y: deviceHeight * 0.75,
+        color: 0x00cccc,
+        name: 'A'
+    }, handleSpineBoyJump);
+
+    createBtn(this, {
+        x: deviceWidth * 0.9,
+        y: deviceHeight * 0.75,
+        color: 0xcc00cc,
+        name: 'B'
+    }, handleSpineBoyShoot);
+
 
     this.physics.add.collider(player, platforms);
 
@@ -85,34 +97,65 @@ function create() {
     resize();
 }
 
-function update() {
-    
+const GetValue = Phaser.Utils.Objects.GetValue;
+var createBtn = function (scene, config, onClick) {
+    var x = GetValue(config, 'x', 0);
+    var y = GetValue(config, 'y', 0);
+    var color = GetValue(config, 'color', 0xffffff);
+    var name = GetValue(config, 'name', '');
 
-    // if (player.getCurrentAnimation().name == 'idle') {
-        // player.clearTrack(0)
-    // }
-    // console.log(player.getCurrentAnimation().name);
-    if (cursors.up.isDown && player.body.touching.down)
-    {   
+    var btn = scene.add.rectangle(x, y, 50, 50, color)
+        .setName(name);
+    scene.add.text(x, y, name, {
+        fontSize: '20pt'
+    })
+        .setOrigin(0.5, 0.5)
+
+    btn.button = new Button(btn, {
+        clickInterval: 500
+    });
+
+    btn.button.on('click', onClick);
+    return btn;
+}
+
+function handleSpineBoyJump(button, gameObject) {
+    if (player.body.touching.down) {
+        player.body.setVelocityX(0);
+        player.body.setVelocityY(-200);
         player.clearTrack(1);
         player.setAnimation(0, 'jump', false, true);
         player.addAnimation(0, 'idle', false);
-        
-        player.body.setVelocityY(-200);
-        
-    } else if (cursors.left.isDown) {
+        isJumping = true;
+    }
+}
+
+function handleSpineBoyShoot(button, gameObject) {
+    if (player.body.touching.down) {
+        player.clearTracks();
+        player.setAnimation(0, 'shoot', false, true);
+        player.addAnimation(0, 'idle', false);
+    }
+}
+
+function update() {
+    
+    if (player.getCurrentAnimation().name != 'jump') {
+        isJumping = false;
+    }
+
+    // if (cursors.left.isDown) {
+    if (joystickCursors.left.isDown) {
         player.body.setVelocityX(-160);
         spineboyRun();
-        player.body.setOffset(player.width, 0) // mamaya
+        player.body.setOffset(player.width, 0);
         player.setScale(-0.15, 0.15);
-        
-        // spineBoy.setFlipX(true);
-    } else if (cursors.right.isDown) {
+    // } else if (cursors.right.isDown) {
+    } else if (joystickCursors.right.isDown) {
         player.body.setVelocityX(160);
         spineboyRun();
-        player.body.setOffset(0, 0) // mamaya
+        player.body.setOffset(0, 0);
         player.setScale(0.15, 0.15);
-        // spineBoy.setFlipX(true);
     } else {
         player.body.setVelocityX(0);
         spineboyIdle();
@@ -120,17 +163,15 @@ function update() {
 }
 
 function spineboyRun() {
-    if (player.getCurrentAnimation().name != 'jump') {
+    if (!isJumping) {
         player.setAnimation(1, 'run', true, true);
-        // .play('run', true, true);
     }
 }
 
 function spineboyIdle() {
-    // if (player.getCurrentAnimation(1).name != 'jump') {
+    if (!isJumping) {
         player.setAnimation(1, 'idle', true, true);
-        // player.play('idle', true, true);
-    // }
+    }
 }
 
 function resize() {
