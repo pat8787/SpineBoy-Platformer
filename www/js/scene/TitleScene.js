7a3171d756fe64c020664436f498758a7a0765b7
PatSmartwave <30307609+pat8787@users.noreply.github.com>
2022-01-31 10:33:10 +0800

    Titlescene + Game Loop Logic (#1)
    * Added GameScene and TitleScene
    Added Win Logic that goes back to TitleScene
    
    Missing: Refactors
    
    * Removed Unnecessary comments

---
+++ www/js/scene/TitleScene.js
@@ -0,0 +1,25 @@
+import Button from '../../node_modules/phaser3-rex-plugins/plugins/button.js';
+import MainIndex from '../index.js';
+
+class TitleScene extends Phaser.Scene {
+    constructor() {
+        super("titleScene");
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        this.add.text(MainIndex.deviceWidth/2, MainIndex.deviceHeight * 0.75, 'Click anywhere to start', {
+            fontSize: '20pt',
+            align: "center"
+        }).setOrigin(0.5, 0.5);
+        
+        this.input.on('pointerup', function (pointer) {
+            this.scene.start('gameScene');
+        }, this);
+    }
+}
+
+export default TitleScene;
